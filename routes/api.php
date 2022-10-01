14c0e3d879a4972cc5fe420a6b1eac030d64db16
victorfrei <vic.frei2@gmail.com>
2022-10-01 16:52:29 -0300

    ✨feat: Criação da API + Rota Clientes (com o CRUD)

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\CustomerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+
+Route::apiResource('customer',CustomerController::class);
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
